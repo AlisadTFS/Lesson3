2bcc758ca770242cc407950584ac68b71fb9f5ab
Alex <virdata@yandex.ru>
2022-03-12 23:11:39 +0800

    Задание №1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,6 +10,44 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество строк массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов массива: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] arrayint = new int[n, m];
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int y = 0; y < m; y++ )
+                {
+                    arrayint[i, y] = rand.Next(100);
+                    Console.Write($" {arrayint[i, y].ToString("d2")} ");
+                }
+                Console.WriteLine();
+            }
+            if (n > m)
+            {
+                n = m;
+            }
+            else
+            {
+                m = n;
+            }            
+            for (int i = 0; i < n; i++)
+            {                
+                for (int y = 0; y < m; y++)
+                {
+                    if ((i == y) || (i == (m-y-1)))
+                    {
+                        Console.Write($" {arrayint[i, y].ToString("d2")} ");
+                    }
+                    else
+                    {
+                        Console.Write("    ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
